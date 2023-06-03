b96e65d5626f505500c4b44772f7df7209f43661
shyamgupta196 <60971003+shyamgupta196@users.noreply.github.com>
2023-06-03 23:42:49 +0530

    added labels but not good, refer labels.js(better)

--- second module/basic.js
+++ second module/basic.js
@@ -3,7 +3,7 @@ var w = 300;
 var h = 200;
 var padding = 2;
 
-var dataset = [5,10,14,20,24,50,60,11,20,49,30,37,25,11];
+var dataset = [5,70,14,20,24,50,60,11,20,49,30,37,25,11];
 
 var svg = d3.select('body').append('svg').attr('width',w).attr('height',h);
 
@@ -16,6 +16,14 @@ function colorpicker(v) {
     }
 }
 
+var num = 30;
+
+function greaterThan(d){
+    if (d>= num){
+        return d;
+    }
+};
+
 svg.selectAll('rect').data(dataset).enter()
     .append('rect')
     .attr({
@@ -25,6 +33,15 @@ svg.selectAll('rect').data(dataset).enter()
         height: function(d,i) {return d;},
         fill: function(d) {return colorpicker(d);}
     });
+
+svg.selectAll('text').data(dataset).enter()
+                                    .append('text')
+                                    .text( function(d) { return greaterThan(d)})
+                                    .attr({
+                                        'x': function(d,i){ return i * (w/dataset.length)},
+                                        'y': function(d,i) { return h -d}
+                                    })
+
 // this graph is great , but I can add labels to it !! 
 // we will add labels to it, in labels.js and labels.html
 
